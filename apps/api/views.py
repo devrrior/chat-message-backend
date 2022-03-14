9ec6c0f1a83f3fcec736a9c61d268d6ef7fdc5e6
Fernando Guerrero <devrrior@gmail.com>
2022-03-14 13:24:26 -0600

    feat(api): customizae jwt token payload

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['firstName'] = user.first_name
+        token['lastName'] = user.last_name
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
