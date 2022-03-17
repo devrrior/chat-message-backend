39165ce1b288ebf2a8343aedec7d8efffc91fd2f
Fernando Guerrero <devrrior@gmail.com>
2022-03-16 23:06:42 -0600

    feat(settings.py): use enviroment variable

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,12 +1,10 @@
 from pathlib import Path
 from datetime import timedelta
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-+fi1==(9+e$x&-ws^=4*(ep^#a6pj2yc^#1^j^65q%y8s(zx!d'
-
-
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 BASE_APPS = [
     'django.contrib.admin',
@@ -36,7 +34,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=3),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
