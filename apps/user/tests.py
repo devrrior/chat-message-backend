39b39ffbb8751571fa54dc3a27b9374de760d12e
Fernando Guerrero <devrrior@gmail.com>
2022-04-01 23:18:49 -0600

    feat(User): add tests for User Model

--- apps/user/tests.py
+++ apps/user/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import User
 
-# Create your tests here.
+class UserTestCase(TestCase):
+    def test_create_user(self):
+        user1 = User.objects.create(email='devrrior@gmail.com', first_name='Fernando', last_name='Guerrero', password='password')
+        self.assertEqual(user1.email, 'devrrior@gmail.com')
+        self.assertEqual(user1.first_name, 'Fernando')
+        self.assertEqual(user1.last_name, 'Guerrero')
