0f66bc98ff6a39bb9e7600e1421552fd52bef331
Fernando Guerrero <devrrior@gmail.com>
2022-04-03 20:37:35 -0500

    feat(project): update middleware for authentication

--- config/asgi.py
+++ config/asgi.py
@@ -4,12 +4,13 @@ from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from apps.chat.routing import websocket_urlpatterns
+from apps.chat.middleware import TokenAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.development')
 
 application = ProtocolTypeRouter(
     {
         # 'http': get_asgi_application,
-        'websocket': AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+        'websocket': TokenAuthMiddleware(URLRouter(websocket_urlpatterns)),
     }
 )
