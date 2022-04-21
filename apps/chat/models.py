a61e3af379e3afccd8934cea89800ff4f8dfaa63
Fernando Guerrero <devrrior@gmail.com>
2022-04-20 23:45:56 -0500

    feat(chat): creat Message model

--- apps/chat/models.py
+++ apps/chat/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from apps.user.models import User
+
+class Message(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'"{self.content}" by {self.author.email}'
+
+    def last_10_messages():
+        return Message.objects.order_by('-created_at').all()[:10]
