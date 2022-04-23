ab9c77c2cce829d682b1b244ec73aef0ebd6a772
Fernando Guerrero <devrrior@gmail.com>
2022-04-22 20:20:15 -0500

    test(chat): create new tests

--- apps/chat/tests.py
+++ apps/chat/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from apps.user.models import User
+
+from .models import Message
+
+
+class MessageTestCase(TestCase):
+    def test_message_creation(self):
+        user1 = User.objects.create(email='devrrior@gmail.com', first_name='Fernando', last_name='Guerrero', password='password')
+        message1 = Message.objects.create(author=user1, content='Hi mate!')
+        self.assertEqual('Hi mate!', message1.content)
+        self.assertEqual(user1, message1.author)
