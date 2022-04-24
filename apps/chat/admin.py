b305185e0bbdfd0d70427857c3fef9af2670d45a
Fernando Guerrero <devrrior@gmail.com>
2022-04-23 23:39:48 -0500

    feat(chat): create contact and chat models

--- apps/chat/admin.py
+++ apps/chat/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Message
+from .models import Contact, Message, Chat
 
+admin.site.register(Contact)
 admin.site.register(Message)
+admin.site.register(Chat)
--- apps/chat/models.py
+++ apps/chat/models.py
@@ -2,15 +2,31 @@ from django.db import models
 
 from apps.user.models import User
 
+class Contact(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='contacts')
+    friends = models.ManyToManyField('self', related_name='friends', blank=True)
+
+    def __str__(self):
+        return self.user.email
+
 
 class Message(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages')
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE, related_name='messages')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'"{self.content}" by {self.author.email}'
+        return f'"{self.content}" by {self.contact.email}'
 
-    def last_10_messages():
-        return Message.objects.order_by('-created_at').all()[:10]
+class Chat(models.Model):
+    participants = models.ManyToManyField(Contact, related_name='chats')
+    messages = models.ManyToManyField(Message, related_name='chats', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Chat between {self.participants.all()}'
+
+    def last_10_messages(self):
+        return self.messages.objects.order_by('-created_at').all()[:10]
