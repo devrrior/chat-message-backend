3e80472a6f6e9a0dbdd47747c5e5c4cc76d28e64
Fernando Guerrero <devrrior@gmail.com>
2022-04-24 21:53:10 -0500

    fix(chat): update models, turn from manyToMany to oneToMany

--- apps/chat/models.py
+++ apps/chat/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from apps.user.models import User
 
+
 class Contact(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='contacts')
     friends = models.ManyToManyField('self', related_name='friends', blank=True)
@@ -10,18 +11,8 @@ class Contact(models.Model):
         return self.user.email
 
 
-class Message(models.Model):
-    contact = models.ForeignKey(Contact, on_delete=models.CASCADE, related_name='messages')
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f'"{self.content}" by {self.contact.email}'
-
 class Chat(models.Model):
     participants = models.ManyToManyField(Contact, related_name='chats')
-    messages = models.ManyToManyField(Message, related_name='chats', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -30,3 +21,16 @@ class Chat(models.Model):
 
     def last_10_messages(self):
         return self.messages.objects.order_by('-created_at').all()[:10]
+
+
+class Message(models.Model):
+    contact = models.ForeignKey(
+        Contact, on_delete=models.CASCADE, related_name='messages'
+    )
+    content = models.TextField()
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='messages', null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'"{self.content}" by {self.contact.user.email}'
