b138556dce3231c27c118f4e38526a395971a038
Fernando Guerrero <devrrior@gmail.com>
2022-04-24 21:53:27 -0500

    test(chat): update tests for new models

--- apps/chat/tests.py
+++ apps/chat/tests.py
@@ -2,15 +2,67 @@ from django.test import TestCase
 
 from apps.user.models import User
 
-from .models import Message
+from .models import Chat, Contact, Message
+
+
+class ContactTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            email='devrrior@gmail.com',
+            first_name='Fernando',
+            last_name='Guerrero',
+            password='password',
+        )
+
+    def test_contact_creation(self):
+        contact = Contact.objects.create(user=self.user)
+        self.assertTrue(isinstance(contact, Contact))
+        self.assertEqual(self.user, contact.user)
+
+
+class ChatTestCase(TestCase):
+    def setUp(self):
+        user1 = User.objects.create(
+            email='devrrior@gmail.com',
+            first_name='Fernando',
+            last_name='Guerrero',
+            password='password1',
+        )
+        self.contact1 = Contact.objects.create(user=user1)
+
+        user2 = User.objects.create(
+            email='k1rie@gmail.com',
+            first_name='Diego',
+            last_name='Gonzalez',
+            password='password2',
+        )
+        self.contact2 = Contact.objects.create(user=user2)
+
+    def test_chat_creation(self):
+        chat = Chat.objects.create()
+
+        chat.participants.set([self.contact1, self.contact2])
+
+        self.assertTrue(isinstance(chat, Chat))
+        self.assertEqual(chat.participants.count(), 2)
+        self.assertEqual(chat.messages.count(), 0)
 
 
 class MessageTestCase(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.user = User.objects.create(email='devrrior@gmail.com', first_name='Fernando', last_name='Guerrero', password='password')
+    def setUp(self):
+        user = User.objects.create(
+            email='devrrior@gmail.com',
+            first_name='Fernando',
+            last_name='Guerrero',
+            password='password',
+        )
+        self.contact = Contact.objects.create(user=user)
+        self.chat = Chat.objects.create()
+        self.chat.participants.set([self.contact])
 
     def test_message_creation(self):
-        message1 = Message.objects.create(author=self.user, content='Hi mate!')
-        self.assertEqual('Hi mate!', message1.content)
-        self.assertEqual(self.user, message1.author)
+        message = Message.objects.create(contact=self.contact, content='Hi mate!', chat=self.chat)
+
+        self.assertTrue(isinstance(message, Message))
+        self.assertEqual('Hi mate!', message.content)
+        self.assertEqual(self.contact, message.contact)
