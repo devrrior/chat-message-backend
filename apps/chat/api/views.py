748a58e64453929fd343aa565bedc7fb09cfc1ef
Fernando Guerrero <devrrior@gmail.com>
2022-04-27 22:56:15 -0500

    feat(chat/api/views): create ChatListAPIView view

--- apps/chat/api/views.py
+++ apps/chat/api/views.py
@@ -1,12 +1,14 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 
-from apps.chat.models import Chat
+from apps.chat.models import Contact
 
 from .serializers import ChatSerializer
 
 
-class ListChatView(ListAPIView):
-    queryset = Chat.objects.all()
+class ChatListAPIView(ListAPIView):
     serializer_class = ChatSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Contact.objects.get(user=self.request.user).chats.all()
