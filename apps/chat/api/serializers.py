a83bd7b6ad71e83c77c9d69189a1cd9e32d569a0
Fernando Guerrero <devrrior@gmail.com>
2022-04-27 22:57:03 -0500

    feat(chat/api/serializers.py): make serializers

--- apps/chat/api/serializers.py
+++ apps/chat/api/serializers.py
@@ -1,5 +1,35 @@
 from rest_framework import serializers
 
+from apps.chat.models import Chat, Contact, Message
+from apps.user.api.serializers import UserSerializer
 
-class ChatSerializer(serializers.Serializer):
-    particpants = serializers.ListField(child=serializers.CharField())
+
+class MessageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Message
+        fields = ('content', 'created_at')
+
+class ContactSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Contact
+        fields = ('user',)
+
+class ChatSerializer(serializers.ModelSerializer):
+    last_message = serializers.SerializerMethodField('get_last_message')
+    receiver = serializers.SerializerMethodField('get_receiver')
+
+    class Meta:
+        model = Chat
+        exclude = ('created_at', 'participants')
+
+    def get_last_message(self, obj):
+        return MessageSerializer(obj.messages.last()).data
+
+    def get_receiver(self, obj):
+        request = self.context.get('request', None)
+        if request is not None and request.user.is_authenticated:
+            if obj.participants.filter(user=request.user).exists():
+                return UserSerializer(obj.participants.exclude(user=request.user).first().user).data
