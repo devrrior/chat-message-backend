bc8fcd585eb263c2c9a687316d2904088903e193
Fernando Guerrero <devrrior@gmail.com>
2022-07-25 22:57:39 -0500

    feat(chat/models): change relationship

--- apps/chat/models.py
+++ apps/chat/models.py
@@ -5,7 +5,7 @@ from apps.user.models import User
 
 class Contact(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='contacts')
-    friends = models.ManyToManyField('self', related_name='friends', blank=True)
+    friends = models.ForeignKey('self', related_name='friends', blank=True)
 
     def __str__(self):
         return self.user.email
@@ -27,9 +27,7 @@ class Chat(models.Model):
 
 
 class Message(models.Model):
-    contact = models.ForeignKey(
-        Contact, on_delete=models.CASCADE, related_name='messages'
-    )
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE, related_name='messages')
     content = models.TextField()
     chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='messages', null=True)
     created_at = models.DateTimeField(auto_now_add=True)
